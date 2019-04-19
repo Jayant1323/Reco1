6ec6db8269de5bcc0d66ad0576150c3a7e40cf6f
ADItyaP999 <adityapujari2@gmail.com>
2019-04-19 13:54:38 +0530

    Changes in splash screen

--- app/src/main/java/com/jay/reco/SplashScreen.java
+++ app/src/main/java/com/jay/reco/SplashScreen.java
@@ -5,6 +5,8 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 
@@ -24,6 +26,11 @@ public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getSupportActionBar().hide();
         setContentView(R.layout.activity_splash_screen);
 
         rellay1 = findViewById(R.id.rellay1);
